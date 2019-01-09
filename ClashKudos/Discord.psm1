21980942d93613c067b52b0dfbe0121a70456cd2
sleeper1320 <sleeper1320@gmail.com>
2019-01-09 13:34:50 -0700

    Added support for rate limiting with partial testing.

--- ClashKudos/Discord.psm1
+++ ClashKudos/Discord.psm1
@@ -4,18 +4,31 @@ function Send-WebhookMessage {
         Performs a call to a Discord webhook.
 
     .DESCRIPTION
-        Validates the parameters passed in for consistency and, if valid, conforms the
-        messages to a Discord consumable format and invokes a web request on the passed
-        in webhook uri.
+        Validates the parameters passed in for consistency and, if valid,
+        conforms the messages to a Discord consumable format and invokes a web
+        request on the passed in webhook uri.
 
-        At the moment, errors received when calling the web request are passed to the
-        calling function. Future revisions of this function may handle errors internally.
+        This function is built to handle Discord webhook rate limitations.
+        Consequently, this function may sleep for a short period of time until
+        the rate limit reset is called. Normal rate limiting lasts for 1-3
+        seconds.
 
-    .PARAMETER hook
+        In extreme cases, where many calls to the webhook are made at once,
+        Discord may throw a 429 error. This error is may require the function
+        sleep before completing this process.
+
+        At the present time, this is a single threaded function and the caller
+        should implement threading if needed.
+
+    .PARAMETER Hook
         A valid, complete Discord webhook to the desired resource.
 
-    .PARAMETER message
+    .PARAMETER Message
         The desired message to send to Discord.
+
+    .PARAMETER RetryCount
+        The number of times to retry after receiving a 429 error before an
+        error is rethrown to the caller. Valid values are between 5 and 0.
 
     .INPUTS
         System.String
@@ -25,7 +38,7 @@ function Send-WebhookMessage {
      #>
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$true)]
         [ValidateScript({
             $output = ''
             $result = $false
@@ -44,15 +57,62 @@ function Send-WebhookMessage {
             #Return the result. At this point, it should be $true.
             $result
         })]
-        [String] $hook,
+        [String] $Hook,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Message,
 
         [Parameter(Mandatory=$false)]
-        [ValidateNotNullOrEmpty()]
-        [String] $message
+        [ValidateRange(0,5)]
+        [Int] $RetryCount=5
     )
     $body = New-Object -TypeName PSObject
     $body | Add-Member -MemberType NoteProperty -Name 'content' -Value $message
     $body = $body |ConvertTo-Json
 
-    Invoke-WebRequest -Uri $hook -Method POST -Body $body | Out-Null
+    #Initialize on first run.
+    if($script:limitRemaining -eq $null) {$script:limitRemaining = 5}
+    if($script:resetTime -eq $null) {$script:resetTime = 0}
+
+    #Get the time to calculate rate limiting below.
+    $currentTime = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
+
+    #Sleep if we will be rate limited.
+    if(($script:limitRemaining -eq 0) -and ($script:resetTime -gt $currentTime)) {
+        $sleep = ($script:resetTime - $currentTime)
+
+        Write-Verbose "Reached rate limit. Sleeping for $sleep seconds."
+        Start-Sleep -Seconds $sleep
+    }
+
+    try {
+        Write-Verbose "Calling webhook"
+
+        $response = Invoke-WebRequest -Uri $hook -Method POST -Body $body
+        $script:limitRemaining = $response.Headers['X-RateLimit-Remaining']
+        $script:resetTime = $response.Headers['X-RateLimit-Reset']
+    } catch [System.Net.WebException] {
+        $response = $_.Exception.Response
+
+        #Throw the error if we've exuasted our retry count.
+        if($RetryCount -eq 0) { throw $_ }
+
+        if($response.StatusCode -eq 429) {
+            Write-Verbose "Received 429 status response."
+
+            # For whatever reason, after a set amount of invokes, Discord suddenly decides
+            # to change the retry time without warning and reset the X-RateLimit-Remaining
+            # header to a non zero value. Zero the limit remaining and set the reset time
+            # to utilize the Retry-After (as per the documentation)
+            $script:limitRemaining = 0
+            $script:resetTime = $currentTime + [Math]::Ceiling($response.Headers['Retry-After']/1000) + 1
+
+            #Recurse to let this function handle sleep logic
+            Send-WebhookMessage -Hook $hook -Message $message -RetryCount ($RetryCount-1)
+        } else {
+            Write-Verbose "Exception was not a 429 error. Re-throwing..."
+            throw $_
+        }
+    }
 }
--- ClashKudos/Tests/Discord.tests.ps1
+++ ClashKudos/Tests/Discord.tests.ps1
@@ -10,22 +10,90 @@ Import-Module $root\..\ClashKudos.psd1 -Force
 InModuleScope 'Discord' {
     Describe 'Discord' {
         Context 'Send-WebhookMessage' {
-            it 'should error out when an invalid hook uri is passed in' {
+            #Variables used in tests
+            $currentTime = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
+
+            #Mocks
+            Mock 'Invoke-WebRequest' {
+                $o = New-Object -TypeName PSObject
+                $o | Add-Member -MemberType NoteProperty -Name 'Headers' -Value @{
+                        'X-RateLimit-Remaining'=1;
+                        'X-RateLimit-Reset'=1
+                    }
+                return $o
+            } -ParameterFilter { $Uri -eq 'http://shortrun.example/' }
+
+            Mock 'Invoke-WebRequest' {
+                throw  [System.Net.WebException]
+            } -ParameterFilter { $Uri -eq 'http://zeroretry.example/' }
+
+            Mock 'Invoke-WebRequest' {
+                throw  [System.Exception]
+            } -ParameterFilter { $Uri -eq 'http://notwebexception.example/' }
+
+            Mock 'Start-Sleep' {throw}
+
+
+            #Actual tests
+            it 'should error out if one or more paramters are invalid.' {
+                {Send-WebhookMessage} | Should throw
+
                 {Send-WebhookMessage -hook 'http: //google.com/' -message 'Test Message'} | Should throw 'The hook passed in is not a valid uri.'
-            }
-            it 'should error out when an invalid message is passed in' {
+
                 {Send-WebhookMessage -hook 'http://testurl.example/' -message $null} | Should throw `
                     'Cannot validate argument on parameter ''message''. The argument is null or empty. Provide an argument that is not null or empty, and then try the command again.'
 
                 {Send-WebhookMessage -hook 'http://testurl.example/' -message ''} | Should throw `
                     'Cannot validate argument on parameter ''message''. The argument is null or empty. Provide an argument that is not null or empty, and then try the command again.'
             }
-            it 'should run through the core functions' {
-                Mock 'Invoke-WebRequest' {}
 
-                Send-WebhookMessage -hook 'http://testurl.example/' -message 'Test Message'
+            it 'should run if X-RateLimit-Remaining > 0 but X-RateLimit-Reset > current time without sleeping' {
+                $script:limitRemaining = 1
+                $script:resetTime = $currentTime + 1
+                Send-WebhookMessage -hook 'http://shortrun.example/' -message 'Test Message'
+            }
 
-                Assert-MockCalled 'Invoke-WebRequest'
+            it 'should run if X-RateLimit-Remaining = 0 but X-RateLimit-Reset < current time without sleeping' {
+                $script:limitRemaining = 0
+                $script:resetTime = $currentTime - 1
+                Send-WebhookMessage -hook 'http://shortrun.example/' -message 'Test Message'
+            }
+
+            <#
+             # For the moment, I'm going to skip this test due to the complications needed to mock
+             # the function correctly. I'm sure there's an easy way to mock it, but I'm not
+             # seeing it and not grasping inheriting the System.Net.WebResponse class correctly.
+             # Adding this as an issue to track it later.
+
+            it 'should retry when a 429 exception is caught' {
+                Send-WebhookMessage -hook 'http://retryrun.example/' -Message 'Test' -RetryCount 5
+
+                Assert-MockCalled 'Invoke-WebRequest' -Exactly -Times 5 -ParameterFilter {
+                    $Uri -eq 'http://retryrun.example/'
+                }
+            }#>
+
+            it 'should throw when RetryCount is 0' {
+                {Send-WebhookMessage -hook 'http://zeroretry.example/' -message 'Test Message' -RetryCount 0} `
+                    | Should throw System.Net.WebException
+
+                Assert-MockCalled 'Invoke-WebRequest' -Exactly -Times 1 -ParameterFilter {
+                    $Uri -eq  'http://zeroretry.example/'
+                }
+            }
+
+            it 'should rethrow the error for non 429 exceptions' {
+                {Send-WebhookMessage -hook 'http://notwebexception.example/' -message 'Test Message'} `
+                    | Should throw System.Exception
+
+                Assert-MockCalled 'Invoke-WebRequest' -Exactly -Times 1 -ParameterFilter {
+                    $Uri -eq  'http://notwebexception.example/'
+                }
+            }
+
+            #Describe block assert verifications.
+            Assert-MockCalled 'Invoke-WebRequest' -Exactly -Times 2 -ParameterFilter {
+                $Uri -eq 'http://shortrun.example/'
             }
         }
     }
